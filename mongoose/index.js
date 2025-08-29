55eaca18418128c0089706e5fbdf3ca04e720a0c
Vivek <vivekghule777@gmail.com>
2025-08-29 17:18:50 +0530

    testing mongoose db connection

---
+++ mongoose/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+main().then( () => {
+     console.log("Connection Sucessful")
+    
+}
+)
+.catch((err) => {
+    console.log(err);
+    
+})
+async function  main(params) {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    
+}
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age:Number
+})
+
+const User = mongoose.model("User",userSchema)
+
+const user1 = new User({
+    nmae: "Adam",
+    email : "demo@gmail.com",
+    age : 18
+})
+
+user1.save();
+
+
