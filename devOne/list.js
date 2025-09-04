373c295823a2968a425ce627e955f189dc1d3c61
Vivek <vivekghule777@gmail.com>
2025-09-04 21:49:47 +0530

    working on db

---
+++ devOne/list.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+let placelistingschema = new mongoose.Schema({
+    title : {
+        type: String,
+        required: true
+    },
+    description : {
+        type: String,
+        required: true
+    },
+    image : {
+        type: String,
+        required: true,
+    },
+    price : {
+        type: Number,
+        required: true
+    },
+    location : {
+        type: String,
+        required: true
+    },
+    country : {
+        type: String,
+        required: true
+    },
+})
+
+module.exports = mongoose.model("placelist", placelistingschema)
